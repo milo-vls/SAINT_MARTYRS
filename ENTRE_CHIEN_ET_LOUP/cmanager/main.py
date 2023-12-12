8dfed50541feca5a6456640f4aaf6f1b035b8ff0
timothee-jean <149187003+timothee-jean@users.noreply.github.com>
2023-12-12 18:00:37 +0100

    Ajout d'une liste déroulante pour la sélection du case, traduction du code en anglais

--- ENTRE_CHIEN_ET_LOUP/cmanager/main.py
+++ ENTRE_CHIEN_ET_LOUP/cmanager/main.py
@@ -6,60 +6,60 @@ import sys
 pygame.init()
 
 # Définir les constantes
-FENETRE_TITRE = "ECEL - Crime manager"
-CARTE_FILENAME = "./map.png"
-COULEUR_FOND = (255, 255, 255)
-TAILLE_REFERENCE = (1920, 1080)
-TAILLE_FENETRE_INIT = (1600, 900)
-CHEMIN_ACCESS_BDD = "../datafiles/DEV/ECELdata.db"
+WINDOW_NAME = "ECEL - Crime manager"
+MAP_FILENAME = "./map.png"
+BG_COLOR = (255, 255, 255)
+REF_SIZE = (1920, 1080)
+INIT_WINDOW_SIZE = (1600, 900)
+DB_ACCESS_PATH = "../datafiles/DEV/ECELdata.db"
 
 # Initialisation SQLite
-init_bdd(CHEMIN_ACCESS_BDD)
+init_bdd(DB_ACCESS_PATH)
 init_tk()
 
 # Charger la carte
-carte = pygame.image.load(CARTE_FILENAME)
-largeur_carte, hauteur_carte = carte.get_rect().size
-coef_redim = TAILLE_REFERENCE[0]/TAILLE_FENETRE_INIT[0]
+map = pygame.image.load(MAP_FILENAME)
+map_width, map_height = map.get_rect().size
+resize_ratio = REF_SIZE[0]/INIT_WINDOW_SIZE[0]
 
 # Calculer le ratio de l'image
-ratio_image = largeur_carte / float(hauteur_carte)
+image_ratio = map_width / float(map_height)
 
-carte_redimensionnee = redimensionner_carte(carte, ratio_image, TAILLE_FENETRE_INIT[0])
+resized_map = map_resize(map, image_ratio, INIT_WINDOW_SIZE[0])
 
 # Initialiser la fenêtre avec la taille de la carte redimensionnée
-fenetre = pygame.display.set_mode(TAILLE_FENETRE_INIT, pygame.RESIZABLE)
-pygame.display.set_caption(FENETRE_TITRE)
+window = pygame.display.set_mode(INIT_WINDOW_SIZE, pygame.RESIZABLE)
+pygame.display.set_caption(WINDOW_NAME)
 
 
 # Boucle principale
-en_cours = True
-while en_cours:
+ongoing = True
+while ongoing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            en_cours = False
+            ongoing = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Lorsque la souris est cliquée, récupérer les coordonnées de la souris
             x, y = pygame.mouse.get_pos()
             # Transforme x,y vers les coordonnées correspondante au format reference
-            x_transfo, y_transfo = int(x*coef_redim), int(y*coef_redim)
+            x_transfo, y_transfo = int(x*resize_ratio), int(y*resize_ratio)
             
             # Afficher la fenêtre d'input
-            afficher_popup(x_transfo, y_transfo)
+            fetch_popup(x_transfo, y_transfo)
 
         # Gérer l'événement de redimensionnement de la fenêtre
         elif event.type == pygame.VIDEORESIZE:
-            nouvelle_largeur = event.w
-            carte_redimensionnee = redimensionner_carte(nouvelle_largeur)
-            fenetre = pygame.display.set_mode((nouvelle_largeur, int(nouvelle_largeur / ratio_image)), pygame.RESIZABLE)
-            coef_redim = TAILLE_REFERENCE[0]/nouvelle_largeur
+            new_width = event.w
+            resized_map = map_resize(new_width)
+            window = pygame.display.set_mode((new_width, int(new_width / image_ratio)), pygame.RESIZABLE)
+            resize_ratio = REF_SIZE[0]/new_width
 
     # Afficher la carte
-    fenetre.fill(COULEUR_FOND)
-    fenetre.blit(carte_redimensionnee, (0, 0))
+    window.fill(BG_COLOR)
+    window.blit(resized_map, (0, 0))
     pygame.display.flip()
 
 # Fermeture
-cloture_bdd()
+close_bdd()
 pygame.quit()
 sys.exit()
